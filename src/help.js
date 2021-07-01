6067390ac8a531db0412b59fc5d87e535ab39ae2
KANTDEV71 <kantmods71@gmail.com>
2021-07-01 03:31:21 -0300

    First commit

---
+++ src/help.js
@@ -0,0 +1,42 @@
+const help = (pushname,prefix) => {
+return`
+eai irmão ${pushname} meu menu abaixo (a) 
+╔┬━━━━━━━━━━━┈ ❄︎
+││    𝐊𝐀𝐍𝐓 𝐕 3
+││⇢ᴅᴏɴᴏ: 𝐊𝐀𝐍𝐓
+││⇢ɴᴜᴍᴇʀᴏ: *wa.me/5511942347051*
+││⇢ɪɴsᴛᴀ:@kant_mods
+││⇢ᴄᴀɴᴀʟ:↓↓↓
+││→OBS DESATIVADO
+││⇢ᴘɪx:71982507697
+││⇢ᴘʀᴇғɪx: ⌈${prefix}⌋
+││⇢𝚒𝚗𝚏𝚘 𝚌𝚘𝚟𝚒𝚍-19: ⌈${prefix}covid19⌋
+││⇢𝚌𝚊𝚜𝚘 𝚎𝚗𝚌𝚘𝚗𝚝𝚛𝚎 𝚎𝚛𝚛𝚘𝚜: ⌈${prefix}bugreport⌋
+│╰────────────┈ ❄︎
+ ⊢───◣𝐊𝐀𝐍𝐓 𝐕 3◥───✯
+│
+│⇨ *${prefix}menufig*
+│⚠️ menu de criação de stickers
+│⇨ *${prefix}plays*
+│⚠️ plays de musica
+│Downloads de audios do YT
+│⇨ *${prefix}jogos*
+│⚠️ comandos de jogos 
+│para zoar com amigos.
+│⇨ *${prefix}menuadm* 
+│ ⚠️ administração de grupos
+│⇨ *${prefix}menu2*  EM PRODUÇÃO
+│ ⚠️ segundo menu
+│⇨ *${prefix}skiller*
+│ ⚠️ comandos que so o dono do bot pode usar 
+│
+╰────⟪𝐾𝐴𝑁𝑇⟫────┈✯
+©     _*Copyright by kant.sad*_
+®     _*dominando tudo⚡*_ 
+╘────────────💫`
+
+}
+
+
+
+exports.help = help
